114045175c289f1837b61b77c036455e524c7350
ejensen0445 <emrykjensen11@hotmail.com>
2018-05-08 20:00:21 +0000

    fast blink

--- gpio_led.py
+++ gpio_led.py
@@ -1,10 +1,20 @@
 from gpiozero import LED
-from time import sleep
+#from time import sleep
+import time
 
 led = LED(17)
 
+
+
+def increasing_blink():
+    for i in range(6000):
+        led.off()
+        i+=1
+    for i in range(500):
+        led.on()
+        i+=1
+    
+
 while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+    increasing_blink()
+
