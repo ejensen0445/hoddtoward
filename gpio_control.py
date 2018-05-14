f4e985add55cee5a6d66cb910b2cccbba4efec30
ejensen0445 <emrykjensen11@hotmail.com>
2018-05-14 20:08:40 +0000

    click = light

--- gpio_control.py
+++ gpio_control.py
@@ -1,11 +1,11 @@
 from gpiozero import LED, Button
 from time import sleep
+from signal import pause
 
 led = LED(19)
 button = Button(16)
 
-while True:
-    button.wait_for_press()
-    led.toggle()
-    sleep(0.25)
-    
+button.when_pressed = led.on
+button.when_released = led.off
+
+pause()
