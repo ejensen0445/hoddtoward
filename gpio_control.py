c7cddb01046a70d83bf101748056a33fd78dad8e
ejensen0445 <emrykjensen11@hotmail.com>
2018-05-14 20:01:45 +0000

    on/off switch

--- gpio_control.py
+++ gpio_control.py
@@ -4,7 +4,8 @@ from time import sleep
 led = LED(19)
 button = Button(16)
 
-button.wait_for_press()
-led.on()
-sleep(3)
-led.off()
+while True:
+    button.wait_for_press()
+    led.toggle()
+    sleep(0.25)
+    
